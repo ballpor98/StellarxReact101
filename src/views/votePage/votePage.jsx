8f79107e9d0d6be9e634be2fb7d1da7c97994065
unknown <chawin.k@ku.th>
2019-02-25 21:49:46 +0700

    add textfield

--- src/views/votePage/votePage.jsx
+++ src/views/votePage/votePage.jsx
@@ -7,18 +7,25 @@ import {
   Row,
   Col
 } from "reactstrap";
-// import stellar from 'stellar-sdk';
+import FormInputs from "components/FormInputs/FormInputs.jsx";
+import StellarSdk from 'stellar-sdk';
 
-// const server = new stellar.Server('https://horizon-testnet.stellar.org');
+const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+StellarSdk.Network.useTestNetwork();
 
 class votePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      secretKey:""
     }
   }
-
+  textHandler = (e) => {
+    console.log(e.target.value);
+    this.setState({secretKey: e.target.value});
+  }
   render() {
+    const secretKey = this.state.secretKey;
     return (
       <div className="content">
         <Row>
@@ -28,6 +35,19 @@ class votePage extends React.Component {
                 <CardTitle tag="h4">Vote page</CardTitle>
               </CardHeader>
               <CardBody>
+              <FormInputs
+                  ncols={["col-md-10 pr-1"]}
+                  proprieties={[
+                    {
+                      label: "Secret Key",
+                      inputProps: {
+                        type: "text",
+                        disabled: false,
+                        onChange: (e) => this.textHandler(e)
+                      }
+                    }
+                  ]}
+                />
               </CardBody>
             </Card>
           </Col>
